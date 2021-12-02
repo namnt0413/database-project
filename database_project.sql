2af771b1da463a6c85079e0b7cf6ac13efc63782
namnt0413 <91653611+namnt0413@users.noreply.github.com>
2021-12-02 23:04:54 +0700

    Update lại database

--- database_project.sql
+++ database_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2021 at 05:34 AM
+-- Generation Time: Dec 02, 2021 at 04:59 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -48,6 +48,7 @@ CREATE TABLE `books` (
   `author` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `price` float NOT NULL,
+  `discount` float NOT NULL,
   `quantity` int(11) NOT NULL,
   `content` mediumtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `publication_date` int(11) NOT NULL,
@@ -71,6 +72,8 @@ CREATE TABLE `orders` (
   `note` text NOT NULL,
   `total` int(11) NOT NULL,
   `created_time` int(11) NOT NULL,
+  `required_time` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
   `last_updated` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -86,6 +89,7 @@ CREATE TABLE `order_details` (
   `book_id` int(11) DEFAULT NULL,
   `quantity` int(11) NOT NULL,
   `price` int(11) NOT NULL,
+  `book_discount` float NOT NULL,
   `created_time` int(11) NOT NULL,
   `last_updated` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -117,7 +121,7 @@ CREATE TABLE `users` (
   `password` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `fullname` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `birthday` int(11) DEFAULT NULL,
-  `phone` int(11) NOT NULL,
+  `phone` varchar(11) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `status` tinyint(4) NOT NULL DEFAULT 1,
   `last_updated` int(11) NOT NULL,
@@ -129,10 +133,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `fullname`, `birthday`, `phone`, `address`, `status`, `last_updated`, `created_time`) VALUES
-(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'administrator', 0, 0, '', 1, 1637421508, 0),
-(2, 'namnt0413', 'f7a174303deddf385177363f75650ad0', 'Nguyễn Tuấn Nam', 987112800, 367099786, 'Thanh Tri, Ha Noi', 1, 1638119155, 1638119155),
-(3, 'test', 'e10adc3949ba59abbe56e057f20f883e', 'Tester', 951778800, 123456789, 'DH Bach Khoa, Hai Ba Trung, Ha Noi', 1, 1638120004, 1638120004),
-(4, 'test2', 'faa8e9ec927099f17d29092bb32cb1bb', 'Tester 2 ', 1582153200, 0, '', 1, 1638120271, 1638120271);
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'administrator', 0, '00000000', '', 1, 1637421508, 0),
+(2, 'namnt0413', 'f7a174303deddf385177363f75650ad0', 'Nguyễn Tuấn Nam', 987112800, '0367099786', 'Vĩnh Quỳnh, Thanh Trì, Hà Nội', 1, 1638371670, 1638119155),
+(3, 'test', 'e10adc3949ba59abbe56e057f20f883e', 'Tester', 951778800, '0123456789', 'DH Bach Khoa, Hai Ba Trung, Ha Noi', 1, 1638120004, 1638120004),
+(7, 'test3', '6efea2f319dffd8c243eb52d267e0966', 'Tester 3th', 946681200, '088889999', 'Phường Bách Khoa, Hai Bà Trưng, Hà Nội', 1, 1638355340, 1638355340),
+(9, 'Bloger', 'e10adc3949ba59abbe56e057f20f883e', 'Người quản lý Blog', 979254000, '0234987650', 'Tokyo,Japan', 1, 1638371204, 1638355591),
+(10, 'Shiper', '25f9e794323b453885f5181f1b624d0b', 'Người giao hàng', 1638313200, '0987654321', 'Nguyễn Xiển,Thanh Xuân, Hà Nội', 1, 1638355730, 1638355730);
 
 --
 -- Indexes for dumped tables
@@ -148,7 +154,8 @@ ALTER TABLE `blogs`
 -- Indexes for table `books`
 --
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `discount` (`discount`);
 
 --
 -- Indexes for table `orders`
@@ -162,7 +169,8 @@ ALTER TABLE `orders`
 ALTER TABLE `order_details`
   ADD PRIMARY KEY (`id`),
   ADD KEY `order_id` (`order_id`),
-  ADD KEY `product_id` (`book_id`);
+  ADD KEY `product_id` (`book_id`),
+  ADD KEY `book_discount` (`book_discount`);
 
 --
 -- Indexes for table `reviewers`
@@ -215,7 +223,19 @@ ALTER TABLE `reviewers`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_details_ibfk_3` FOREIGN KEY (`book_discount`) REFERENCES `books` (`discount`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
