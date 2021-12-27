c13d007f7cd73740da484de649e84da9ea69308b
hason <hssh18121@gmail.com>
2021-12-27 18:21:39 +0700

    Them giao dien cho file dang nhap va dang ky

--- login.php
+++ login.php
@@ -24,22 +24,177 @@ and open the template in the editor.
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="assets/css/fontawesome.css">
         <style>
+            /* Login form */
+            * {
+                padding: 0;
+                margin: 0;
+                box-sizing: border-box;
+                color: #495057;
+            }
+
+            .content-container {
+                position: relative;
+                height: 100vh;
+                 background-image: linear-gradient(rgba(233, 236, 239, 0.603), rgba(233, 236, 239, 0.603));
+                 background-image: linear-gradient(rgba(34, 34, 34, 0.603), rgba(34, 34, 34, 0.603)), url(assets/image/login-theme.jpg);
+                background-size: cover;
+            }
+
+            h1 {
+                font-size: 1.8rem;
+               margin-bottom: 2rem;
+               font-weight: 700;
+            }
+
+           
+
             .box-content{
-                margin: 0 auto;
-                width: 800px;
+                background: rgb(256,256,256,0.8);
+                position: absolute;
+                box-shadow: 0 20px 30px 0 rgba(0, 0, 0, 0.07);
+                border-radius: 12px;
+                top: 50%;
+                right: 50%;
                 border: 1px solid #ccc;
-                text-align: center;
                 padding: 20px;
+                transform: translate(50%, -40%);
+                height: 450px;
             }
+
             #user_login form{
-                width: 200px;
-                margin: 40px auto;
+                width: 380px;
+                
+                display: flex;
+                gap: 0.8rem;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                margin-bottom: 2rem;
             }
-            #user_login form input{
+            form input{
+                padding: 10px;
+                width: 360px;
+                height: 40px;
                 margin: 5px 0;
+                border-radius: 9px;
+                border: 1px solid #ccc;
             }
+
+            .dang-nhap-button {
+                font-size: 15px;
+                font-weight: 600;
+                background-color: #27ae60;
+                color: #fff;
+                text-decoration: none;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                cursor: pointer;
+                border-radius: 9px;
+            }
+
+            .dang-nhap-button:hover, .dang-nhap-button:active {
+                background-color: #219150;
+            }
+            
+            nav a:link, nav a:visited {
+                font-size: 20px;
+                margin-left: 20px;
+                color: #fff;
+                text-decoration: none;
+                text-transform: uppercase;
+                font-weight: 500;
+                display: inline-block;
+                border-radius: 100px;
+                display: flex;
+                border-radius: 100px;
+                align-items: center;
+                gap: 4px;
+            }
+
+            nav a:hover, nav a:active {
+                text-decoration: underline;
+            }
+
+            nav {
+                position: absolute;
+                width: 100%;
+                height: 60px;
+                top: 0;
+                /* background-color: rgba(39,174,96,0.8); */
+                display: flex;
+                align-items: center;
+            }
+
+            .click-dang-ky {
+                margin-left: 10px;
+                margin-right: 10px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                gap: 3px;
+                margin-top: 50px;
+                margin-bottom: 20px;
+            }
+
+            .click-dang-ky a:link, .click-dang-ky a:visited {
+                font-size: 15px;
+                font-weight: 600;
+                background-color: #f59f00;
+                color: #fff;
+                text-decoration: none;
+                display: flex;
+                width: 360px;
+                height: 40px;
+                cursor: pointer;
+                justify-content: center;
+                align-items: center;
+                border-radius: 9px;
+            }
+
+            .click-dang-ky a:hover, .click-dang-ky a:active {
+                background-color: #f08c00;
+            }
+
+            /* Form bao loi dang nhap khong thanh cong */
+            .back-button:link, .back-button:visited {
+                font-size: 15px;
+                font-weight: 600;
+                background-color: #27ae60;
+                color: #fff;
+                text-decoration: none;
+                display: flex;
+                height: 40px;
+                cursor: pointer;
+                justify-content: center;
+                align-items: center;
+                border-radius: 9px;
+                text-align: center;
+                
+            }
+
+            .back-button:hover, .back-button:active {
+                background-color: #219150;
+            }
+            #login-notify {
+                width: 400px;
+                height: 300px;
+            }
+            h4 {
+                font-size: 1rem;
+                margin-bottom: 100px;
+            }
+
+            svg {
+            stroke: #fff;
+            width: 25px;
+            height: 25px;
+            }
+            
         </style>
     </head>
+    
     <body>
         <?php
         session_start();
@@ -58,10 +213,12 @@ and open the template in the editor.
             mysqli_close($con);
             if ($error !== false || $result->num_rows == 0) {
                 ?>
-                <div id="login-notify" class="box-content">
-                    <h1>Thông báo</h1>
-                    <h4><?= !empty($error) ? $error : "Thông tin đăng nhập không chính xác" ?></h4>
-                    <a href="./login.php">Quay lại</a>
+                <div class="content-container">
+                    <div id="login-notify" class="box-content">
+                        <h1>Thông báo</h1>
+                        <h4><?= !empty($error) ? $error : "Thông tin đăng nhập không chính xác" ?></h4>
+                        <a href="./login.php" class="back-button">Quay lại</a>
+                    </div>
                 </div>
                 <?php
                 exit;
@@ -72,22 +229,31 @@ and open the template in the editor.
             //include './facebook_source.php';
             //include './google_source.php';
         if (empty($_SESSION['current_user'])) { ?>
-            <div id="user_login" class="box-content">
-                <h1>Đăng nhập tài khoản</h1>
-                <form action="./login.php" method="Post" autocomplete="off">
-                    <label>Username</label></br>
-                    <input type="text" name="username" value="" /><br/>
-                    <label>Password</label></br>
-                    <input type="password" name="password" value="" /></br>
-                    <br>
-                    <input type="submit" value="Đăng nhập" /><br/>
-                    <br><p>Chưa có tài khoản?</p>
-                    <a href="./register.php">Click vào đây để đăng ký</a>
-                </form>
-                 <h2>Hoặc đăng nhập với mạng xã hội</h2>
-                
+            <div class="content-container">
+                <nav class="nav-bar">
+                <a href="./index.php" class="link-trang-chu">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                        </svg> Về trang chủ</a>
+                </nav>
+                <div id="user_login" class="box-content">
+                    <h1>Đăng nhập tài khoản</h1>
+                    <form action="./login.php" method="Post" autocomplete="off">
+                        <!-- <label>Username</label></br> -->
+                        <input type="text" name="username" value="" placeholder="Tên tài khoản"/>
+                        <!-- <label>Password</label></br> -->
+                        <input type="password" name="password" value="" placeholder="Mật khẩu"/>
+                    
+                        <input type="submit" value="Đăng nhập" class="dang-nhap-button"/>
+                        
+                    </form>
+                    <div class="click-dang-ky">
+                        <p>Chưa có tài khoản?</p>
+                        <a href="./register.php">Đăng ký ngay &rarr;</a>
+                    </div>
+                    
+                </div>
             </div>
-
 
             <!-- NEU NHU DANG NHAP THANH CONG -->
             <?php
--- register.php
+++ register.php
@@ -6,24 +6,114 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>Dang ki tai khoan</title>
+        <title>Đăng ký tài khoản</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="https://site.aace.org/wp-content/uploads/2018/04/Book-Icon.png" type="image/x-icon"/>
         <style>
+            * {
+                padding: 0;
+                margin: 0;
+                box-sizing: border-box;
+                color: #495057;
+                font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+            }
+            h1 {
+                margin-bottom: 50px;
+            }
+            .content-container {
+                position: relative;
+                height: 100vh;
+                 background-image: linear-gradient(rgba(233, 236, 239, 0.603), rgba(233, 236, 239, 0.603));
+                 background-image: linear-gradient(rgba(34, 34, 34, 0.603), rgba(34, 34, 34, 0.603)), url(assets/image/login-theme.jpg);
+                background-size: cover;
+
+            }
             .box-content{
                 margin: 0 auto;
-                width: 800px;
+                width: 520px;
                 border: 1px solid #ccc;
                 text-align: center;
                 padding: 20px;
+                box-shadow: 0 20px 30px 0 rgba(0, 0, 0, 0.07);
+                border: 1px solid #ccc;
+                position: absolute;
+                box-shadow: 0 20px 30px 0 rgba(0, 0, 0, 0.07);
+                border-radius: 12px;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                background: rgb(256,256,256,0.9);
             }
-            #user_register form{
-                width: 200px;
-                margin: 40px auto;
+           
+
+            .input-block {
+                width: 100%;
+                display: flex;
+                align-items: center;
+                gap: 20px;
+                margin-bottom: 30px;
+                position: relative;
             }
-            #user_register form input{
-                margin: 5px 0;
+
+            .input-area {
+                height: 30px;
+                width: 350px;
+                position: absolute; 
+                right: 10px;
+                border-radius: 9px;
+                border: 1px solid #ccc;
+                padding: 5px;
+            }
+            
+            .button {
+                height: 40px;
+                width: 120px;
+                font-size: 14px;
+                font-weight: 600;
+                background-color: #27ae60;
+                color: #fff;
+                text-decoration: none;
+                cursor: pointer;
+                border-radius: 9px;
+                border: 0;
+                padding: 5px 10px;
+            }
+
+            .button:hover, .button:active {
+                background-color: #219150;
+            }
+
+            nav a:link, nav a:visited {
+                font-size: 20px;
+                margin-left: 20px;
+                color: #fff;
+                text-decoration: none;
+                text-transform: uppercase;
+                font-weight: 500;
+                display: flex;
+                border-radius: 100px;
+                align-items: center;
+                gap: 4px;
+            }
+
+            nav a:hover, nav a:active {
+                text-decoration: underline;
+            }
+            nav {
+                position: absolute;
+                width: 100%;
+                height: 60px;
+                top: 0;
+                /* background-color: rgba(39,174,96,0.8); */
+                display: flex;
+                align-items: center;
+            }
+
+            svg {
+            stroke: #fff;
+            width: 25px;
+            height: 25px;
             }
         </style>
     </head>
@@ -54,22 +144,28 @@ and open the template in the editor.
                 }
                 if ($error !== false) {
                     ?>
-                    <div id="error-notify" class="box-content">
-                        <h1>Thông báo</h1>
-                        <h4><?= $error ?></h4>
-                        <a href="./register.php">Quay lại</a>
+                    <div class="content-container">
+                        <div id="error-notify" class="box-content">
+                            <h1>Thông báo</h1>
+                            <h4><?= $error ?></h4>
+                            <a href="./register.php">Quay lại</a>
+                        </div>
                     </div>
                 <?php } else { ?>
-                    <div id="edit-notify" class="box-content">
-                        <h1><?= ($error !== false) ? $error : "Đăng ký tài khoản thành công" ?></h1>
-                        <a href="./login.php">Mời bạn đăng nhập</a>
+                    <div class="content-container">
+                        <div id="edit-notify" class="box-content">
+                            <h1><?= ($error !== false) ? $error : "Đăng ký tài khoản thành công" ?></h1>
+                            <a href="./login.php">Mời bạn đăng nhập</a>
+                        </div>
                     </div>
                 <?php } ?>
             <?php } 
             else { ?>
-                <div id="edit-notify" class="box-content">
-                    <h1>Vui lòng nhập đủ thông tin để đăng ký tài khoản</h1>
-                    <a href="./register.php">Quay lại đăng ký</a>
+                <div class="content-container">
+                    <div id="edit-notify" class="box-content">
+                        <h1>Vui lòng nhập đủ thông tin để đăng ký tài khoản</h1>
+                        <a href="./register.php">Quay lại đăng ký</a>
+                    </div>
                 </div>
                 <?php
             }
@@ -77,29 +173,53 @@ and open the template in the editor.
         // Neu khong co action =reg thi hien thi form dang ki
         else {
             ?>
-            <div id="user_register" class="box-content">
-                <h1>Đăng ký tài khoản</h1>
-                <form action="./register.php?action=reg" method="Post" autocomplete="off">
-                    <label>Username</label></br>
-                    <input type="text" name="username" value=""><br/>
-                    <label>Password</label></br>
-                    <input type="password" name="password" value="" /></br>
-                    <label>Họ tên</label></br>
-                    <input type="text" name="first_name" value="" /><br/>
-                    <label>Tên</label></br>
-                    <input type="text" name="last_name" value="" /><br/>
-                    <label>Ngày sinh (DD-MM-YYYY)</label></br>
-                    <input type="text" name="birthday" value="" /><br/>
-                    <label>Số điện thoại</label></br>
-                    <input type="text" name="phone" value="" /><br/>
-                    <label>Địa chỉ</label></br>
-                    <input type="text" name="address" value="" /><br/>
-                    <label>Email</label></br>
-                    <input type="text" name="email" value="" /><br/>
-                    </br>
-                    </br>
-                    <input type="submit" value="Đăng ký"/>
-                </form>
+            <div class="content-container">
+                <nav class="nav-bar">
+                    <a href="./index.php" class="link-trang-chu">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                        </svg> Về trang chủ</a>
+                </nav>
+                <div id="user_register" class="box-content">
+                    <h1>Đăng ký tài khoản</h1>
+                    <form action="./register.php?action=reg" method="Post" autocomplete="off">
+                        <div class="input-block">
+                            <label>Username</label></br>
+                            <input type="text" name="username" value="" class="input-area" placeholder="Điều kiện cho trường nhập vào"><br/>
+                        </div>
+                        <div class="input-block">
+                            <label>Password</label></br>
+                            <input type="password" name="password" value="" class="input-area" placeholder="Điều kiện cho trường nhập vào"/></br>
+                        </div>
+                        <div class="input-block">
+                            <label>Họ và Đệm</label></br>
+                            <input type="text" name="first_name" value="" class="input-area" placeholder="Điều kiện cho trường nhập vào"/><br/>
+                        </div>
+                        <div class="input-block">
+                            <label>Tên</label></br>
+                            <input type="text" name="last_name" value="" class="input-area" placeholder="Điều kiện cho trường nhập vào"/><br/>
+                        </div>
+                        <div class="input-block">
+                            <label>Ngày Sinh </label></br>
+                            <input type="text" name="birthday" value="" class="input-area" placeholder="Phải có dạng (DD-MM-YYYY)"/><br/>
+                        </div>
+                        <div class="input-block">
+                            <label>Số Điện Thoại</label></br>
+                            <input type="text" name="phone" value="" class="input-area" placeholder="Điều kiện cho trường nhập vào"/><br/>
+                        </div>
+                        <div class="input-block">
+                            <label>Địa Chỉ</label></br>
+                            <input type="text" name="address" value="" class="input-area" placeholder="Điều kiện cho trường nhập vào"/><br/>
+                        </div>
+                        <div class="input-block">
+                            <label>Email</label></br>
+                            <input type="text" name="email" value="" class="input-area" placeholder="Điều kiện cho trường nhập vào"/><br/>
+                        </div>
+                        </br>
+                        </br>
+                        <input type="submit" value="Đăng ký" class="button"/>
+                    </form>
+                </div>
             </div>
             <?php
         }
