da81f3dd7f0bba505fc65964051599c39e0e3dff
hason <hssh18121@gmail.com>
2022-01-14 14:48:58 +0700

    Hoàn thành sửa (thêm giao diện cơ bản) cho trang admin

--- admin/book_delete_author.php
+++ admin/book_delete_author.php
@@ -21,10 +21,7 @@
         include 'admin_navbar.php';
     ?>
         <!-- MAIN CONTENT -->
-            <div class="main-content">
-                <div class="section__content section__content--p30">
-                    <div class="container-fluid">
-                        <div class="row">
+            
                         <?php
                         $error = false;
                         if (isset($_GET['id']) && !empty($_GET['id'])) {
@@ -37,23 +34,24 @@
                             mysqli_close($con);
                             if ($error !== false) {
                                 ?>
-                                <div id="error-notify" class="box-content">
-                                    <h2>Thông báo</h2>
-                                    <h4><?= $error ?></h4>
-                                    <a href="javascript:window.history.go(-1)">Quay lại</a>
+                                <div class="content-container">
+                                    <div id="error-notify" class="box-content">
+                                        <h2>Thông báo</h2>
+                                        <h4><?= $error ?></h4>
+                                        <a class="link-button" href="javascript:window.history.go(-1)">Quay lại</a>
+                                    </div>
                                 </div>
                             <?php } else { ?>
-                                <div id="success-notify" class="box-content">
-                                    <h2>Xóa tác giả cuốn sách thành công</h2>
-                                    <a href="javascript:window.history.go(-1)">Quay lại</a>
+                                <div class="content-container">
+                                    <div id="success-notify" class="box-content">
+                                        <h2>Xóa tác giả cuốn sách thành công</h2>
+                                        <a class="link-button" href="javascript:window.history.go(-1)">Quay lại</a>
+                                    </div>
                                 </div>
                             <?php } ?>
                         <?php } ?>
  
-                        </div>
-                    </div>
-                </div>
-            </div>
+                       
 
     <!-- Jquery JS-->
     <script src="vendor/jquery-3.2.1.min.js"></script>
@@ -92,6 +90,61 @@
     <?php } ?>   <!-- end else -->
 
 </body>
+<style>
+     * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+                font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+                color: #495057;
+            }
+            h2 {
+                margin-bottom: 20px;
+            }
+            .content-container {
+                margin-top: 5rem;
+                position: relative;
+                height: 90vh;
+                 /* background-image: linear-gradient(rgba(233, 236, 239, 0.603), rgba(233, 236, 239, 0.603));
+                 background-image: linear-gradient(rgba(34, 34, 34, 0.603), rgba(34, 34, 34, 0.603)), url(assets/image/login-theme.jpg);
+                background-size: cover; */
+                background-image: linear-gradient(#f4f4f4, #7ac187);
 
+            }
+            .box-content{
+                margin: 0 auto;
+                width: 500px;
+                
+                text-align: center;
+                padding: 20px;
+                box-shadow: 0 20px 30px 0 rgba(0, 0, 0, 0.07);
+                border: 1px solid #ccc;
+                position: absolute;
+                box-shadow: 0 20px 30px 0 rgba(0, 0, 0, 0.07);
+               
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                background: rgb(256,256,256,0.9);
+            }
+
+            .link-button:link, .link-button:visited {
+                display: inline-block; 
+                text-decoration: none; 
+                font-size: 17px;
+                font-weight: 600;
+                background-color: #f59f00;
+                color: #fff;
+                text-decoration: none;
+                cursor: pointer;
+                border-radius: 5px;
+                border: 0;
+                padding: 5px 10px;
+            }
+
+            .link-button:hover, .link-button:active {
+                background-color: #f08c00;
+            }
+</style>
 </html>
 <!-- end document-->
